c6f75941a4a303e4f1cbd73ddcc4b629282f57fe
Charles Averill <charlesaverill20@gmail.com>
2022-02-06 21:20:59 -0600

    First attempt at index.html for all subdirs

--- gen_tree.sh
+++ gen_tree.sh
@@ -1,1 +1,8 @@
-tree -H . -o index.html
+#tree -H . -o index.html
+FoundDirs=$(ls -d Volume_*/ Volume_*/*)
+RootDir=$PWD
+for value in $FoundDirs
+do
+	cd "$RootDir/$value"
+	tree -H . -o index.html
+done
